8cedd871d919603af04e3e58fe8031220f882775
Tony <tonyguarino1489@gmail.com>
2023-06-25 21:54:53 -0400

    removed password from string

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 mongoose.connect(
-  'mongodb+srv://tg1489:RUBYruby2808!!@cluster0.hr6x6af.mongodb.net/bootcamp-social-media-backend'
+  `mongodb+srv://tg1489:${process.env.PASSWORD}@cluster0.hr6x6af.mongodb.net/bootcamp-social-media-backend`
 );
 
 module.exports = mongoose.connection;
